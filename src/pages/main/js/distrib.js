6e4d56f946ff46dac4e9921e3edc99f3213c124c
Vlasov Sergey <sergeniy@ya.ru>
2019-03-19 12:50:48 +0300

    main #3

--- src/pages/main/js/distrib.js
+++ src/pages/main/js/distrib.js
@@ -1,5 +1,9 @@
 import Choices from 'choices.js';
 import citiesRussia from '../../../images/map/russia';
+import moscow from '../../../images/map/moscow';
+import piter from '../../../images/map/piter';
+import abakan from '../../../images/map/abakan';
+import armavir from '../../../images/map/armavir';
 import citiesBelarus from '../../../images/map/belarus';
 import citiesKazakhstan from '../../../images/map/kazakhstan';
 
@@ -11,7 +15,7 @@ export default class {
   }
 
   addEvents() {
-    new Choices(this.distribCitySelect, {
+    window.selectCities = new Choices(this.distribCitySelect, {
       itemSelectText: '',
       noResultsText: 'Не найдено'
     });
@@ -25,11 +29,11 @@ export default class {
       var myMap = new ymaps.Map('map', {
           center: [55.4707759, 37.6800719],
           zoom: 8,
-          controls: []
+          // controls: []
         }),
         objectManager = new ymaps.ObjectManager({
           clusterize: true,
-          gridSize: 32,
+          gridSize: 48,
           clusterDisableClickZoom: true
         });
       objectManager.objects.options.set({
@@ -42,6 +46,24 @@ export default class {
       //запрет зума по скроллу
       myMap.behaviors.disable('scrollZoom');
 
+      const distribCitySelect = document.querySelector('.js-distrib__select');
+
+      distribCitySelect.addEventListener('change', (event) => {
+        if (event.detail.value) {
+          fetch('../../../images/map/' + event.detail.value + '.json')
+          .then(res => res.json())
+          .then(data => {
+            objectManager.removeAll();
+            objectManager.add(data);
+            //центрирование по меткам
+            setTimeout(() => {
+              myMap.setBounds(objectManager.getBounds());
+            }, 100);
+          });
+        }
+      });
+
+
       const distribCountries = document.querySelectorAll('.js-distrib__country-item');
 
       distribCountries.forEach(country => {
@@ -49,6 +71,13 @@ export default class {
           if (country.classList.contains('active')) return;
           document.querySelector('.js-distrib__country-item.active').classList.remove('active');
           country.classList.add('active');
+
+          window.selectCities.clearStore();
+          window.selectCities.setChoices([
+            {value: 'moscow', label: 'Moscow', selected: true},
+            {value: 'Two', label: 'Label Two'},
+            {value: 'Three', label: 'Label Three'},
+          ], 'value', 'label', false);
 
           let countryName = event.currentTarget.dataset.country;
           objectManager.removeAll();
