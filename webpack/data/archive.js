8710bfbef957471ff260da1ba99c7aab91673039
Vlasov Sergey <sergeniy@ya.ru>
2019-06-13 00:14:40 +0300

    pages

---
+++ webpack/data/archive.js
@@ -0,0 +1,45 @@
+module.exports = [
+  {
+    subtitle: 'Артикул: а16581',
+    title: 'BS-ALARIS-1-FU1-230/230-2Bt',
+    img: '/images/catalog/catalog-favourite-pic4.png',
+    price: '238 300',
+    advdescr: '№ 1 Автономная система аварийного освещения с функцией TELECONTROL. Пульт аварийного освещения',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+  },
+  {
+    subtitle: 'Артикул: а16581',
+    title: 'BS-ALARIS-1-FU1-230/230-2Bt',
+    img: '/images/catalog/catalog-favourite-pic4.png',
+    advdescr: '№ 1 Автономная система аварийного освещения с функцией TELECONTROL. Пульт аварийного освещения',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+  },
+  {
+    subtitle: 'Артикул: а16581',
+    title: 'BS-ALARIS-1-FU1-230/230-2Bt',
+    img: '/images/catalog/catalog-favourite-pic4.png',
+    price: '238 300',
+    advdescr: '№ 1 Автономная система аварийного освещения с функцией TELECONTROL. Пульт аварийного освещения',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+  },
+  {
+    subtitle: 'Артикул: а16581',
+    title: 'BS-ALARIS-1-FU1-230/230-2Bt',
+    img: '/images/catalog/catalog-favourite-pic4.png',
+    advdescr: '№ 1 Автономная система аварийного освещения с функцией TELECONTROL. Пульт аварийного освещения',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+  },
+
+];
---
+++ webpack/data/autonomous.js
@@ -0,0 +1,366 @@
+module.exports = [
+  {
+    title: 'Офис комфорт 40 вт',
+    img: '/images/catalog/catalog-favourite-pic4.png',
+    price: '238 300',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+    parts: [
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item001.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item002.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item003.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item004.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item005.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item006.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item007.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item008.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item009.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item010.png'
+      }
+    ]
+
+  },
+  {
+    title: 'Офис СВО ip54 33Вт',
+    img: '/images/catalog/catalog-favourite-pic5.png',
+    price: '238 300',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+    parts: [
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item001.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item002.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item003.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item004.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item005.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item006.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item007.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item008.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item009.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item010.png'
+      }
+    ]
+
+  },
+  {
+    title: 'офис 33 Вт',
+    img: '/images/catalog/catalog-favourite-pic6.png',
+    price: '238 300',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+    parts: [
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item001.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item002.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item003.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item004.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item005.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item006.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item007.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item008.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item009.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item010.png'
+      }
+    ]
+
+  },
+  {
+    title: 'офис 33 Вт (ЧЕРЕПАХА)',
+    img: '/images/catalog/catalog-favourite-pic1.png',
+    price: '238 300',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+    parts: [
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item001.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item002.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item003.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item004.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item005.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item006.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item007.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item008.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item009.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item010.png'
+      }
+    ]
+
+  },
+  {
+    title: 'ТИТАН 33 Вт',
+    img: '/images/catalog/catalog-favourite-pic2.png',
+    price: '238 300',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+    parts: [
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item001.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item002.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item003.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item004.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item005.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item006.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item007.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item008.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item009.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item010.png'
+      }
+    ]
+
+  },
+  {
+    title: 'Высота 33 Вт,<br> 100 Вт',
+    img: '/images/catalog/catalog-favourite-pic3.png',
+    price: '238 300',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+    parts: [
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item001.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item002.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item003.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item004.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item005.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item006.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item007.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item008.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item009.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item010.png'
+      }
+    ]
+
+  },
+  {
+    title: 'Струна 35 Вт, 47 Вт,<br> 60 Вт.',
+    img: '/images/catalog/catalog-favourite-pic4.png',
+    price: '238 300',
+    link: {
+      href: 'javascript:void(0);',
+      title: 'подробнее',
+    },
+    parts: [
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item001.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item002.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item003.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item004.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item005.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item006.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item007.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item008.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item009.png'
+      },
+      {
+        label: 'Описание иконки',
+        icon: 'images/lights/lights-item010.png'
+      }
+    ]
+
+  },
+];
