9f5f8b444629b6c1215a55286769666246aab182
Vlasov Sergey <sergeniy@ya.ru>
2020-02-06 21:23:13 +0300

    pages

--- src/components/components.js
+++ src/components/components.js
@@ -1,6 +1,6 @@
 import objectFitImages from 'object-fit-images';
-import 'lazysizes/plugins/bgset/ls.bgset';
-import lazySizes from 'lazysizes';
+// import 'lazysizes/plugins/bgset/ls.bgset';
+// import 'lazysizes';
 
 document.addEventListener('DOMContentLoaded', event => {
 
