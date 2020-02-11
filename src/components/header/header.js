6d722ac15d442067fa9e2fde4e212438fcf40abb
Vlasov Sergey <sergeniy@ya.ru>
2020-02-11 20:03:43 +0300

    pages

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,12 +2,23 @@ export default class {
   constructor() {
     this.parentMenu = document.querySelectorAll('.js-header__menu-parent');
     this.parentTopMenu = document.querySelectorAll('.js-header__top-parent');
+    this.mobileMenu = document.querySelector('.js-header-mobile');
+    this.mobileMenuBtn = document.querySelector('.header__middle .js-header__burger');
+    this.mobileMenuMainBtn = this.mobileMenu.querySelector('.js-header__burger');
     this.addEvents();
   }
 
   addEvents() {
     this.menuToggle();
     this.topMenuToggle();
+
+    this.mobileMenuBtn.addEventListener('click', () => {
+      this.mobileMenuToggle();
+    });
+
+    this.mobileMenuMainBtn.addEventListener('click', () => {
+      this.mobileMenuToggle();
+    });
   }
 
   menuToggle() {
@@ -25,6 +36,7 @@ export default class {
       });
     });
   }
+
   topMenuToggle() {
     this.parentTopMenu.forEach(menu => {
       menu.addEventListener('click', () => {
@@ -41,4 +53,15 @@ export default class {
     });
   }
 
+  mobileMenuToggle() {
+    if (this.mobileMenu.classList.contains('is-open')) {
+      this.mobileMenu.classList.remove('is-open');
+      this.mobileMenuBtn.classList.remove('is-active');
+      this.mobileMenuMainBtn.classList.remove('is-active');
+    } else {
+      this.mobileMenu.classList.add('is-open');
+      this.mobileMenuBtn.classList.add('is-active');
+      this.mobileMenuMainBtn.classList.add('is-active');
+    }
+  }
 }
