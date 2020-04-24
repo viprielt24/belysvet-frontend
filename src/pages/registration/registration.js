3b2a832c25458106f884bafde3e8df2bfe269703
Sergey Vlasov <sergeniy@ya.ru>
2020-04-24 14:29:33 +0300

    new registration

--- src/pages/registration/registration.js
+++ src/pages/registration/registration.js
@@ -6,10 +6,13 @@ export default class {
     this.selectCountry = document.querySelector('.js-select-country');
     this.selectCity = document.querySelector('.js-select-city');
     this.selectOrg = document.querySelector('.js-select-org');
+    this.selectCustomer = document.querySelector('.js-select-customer');
 
     this.regTags = document.querySelectorAll('.js-reg-tagline .tag');
     this.nondistribWrap = document.querySelector('.js-registration-nondistrib');
     this.distribWrap = document.querySelector('.js-registration-distrib');
+    this.customerWrap = document.querySelector('.js-registration-customer');
+    this.customerInput = document.querySelector('.js-registration-customer-other');
 
     this.addEvents();
     this.tagToggle();
@@ -43,7 +46,15 @@ export default class {
       renderSelectedChoices: true,
       searchPlaceholderValue: 'Поиск',
     }).disable();
-
+    const selectCustomerInst = new Choices(this.selectCustomer, {
+      placeholder: false,
+      searchFloor: 2,
+      shouldSort: false,
+      itemSelectText: '',
+      noResultsText: 'Не найдено',
+      renderSelectedChoices: true,
+      searchPlaceholderValue: 'Поиск',
+    });
 
     selectCountryInst.passedElement.element.addEventListener('change', (event) => {
       selectCityInst.setChoices([
@@ -62,22 +73,75 @@ export default class {
         {value: '67', label: ' ООО «Элком-Электро»'},
       ], 'value', 'label', true).enable();
     });
+
+    selectCustomerInst.setChoices([
+      {value: '51', label: 'Монтажная организация'},
+      {value: '52', label: 'Генеральный подрядчик'},
+      {value: '53', label: 'Заказчик'},
+      {value: '54', label: 'Инвестор'},
+      {value: '55', label: 'Эксплуатационная организация'},
+      {value: '56', label: 'Конечный потребитель'},
+      {value: 'drugoe', label: 'Другое'},
+    ], 'value', 'label', true).enable();
+
+    selectCustomerInst.passedElement.element.addEventListener('change', (event) => {
+      const selectValue = event.detail.value;
+      this.customerInput.disabled = selectValue !== 'drugoe';
+    })
   }
 
   tagToggle() {
     this.regTags.forEach(tag => {
       tag.addEventListener('click', () => {
-        tag.classList.toggle('tag--active');
+        const activeClassCheck = document.querySelector('.js-reg-tagline .tag.tag--active');
 
-        if (tag.classList.contains('js-tag-distrib')) {
-          if (tag.classList.contains('tag--active')) {
+        if (tag.classList.contains('tag--active')) {
+
+          tag.classList.remove('tag--active');
+
+          this.nondistribWrap.style.display = 'block';
+          this.distribWrap.style.display = 'none';
+          this.customerWrap.style.display = 'none';
+
+        } else {
+          if (activeClassCheck !== null) {
+            activeClassCheck.classList.remove('tag--active');
+          }
+
+          tag.classList.add('tag--active');
+
+          if (tag.classList.contains('js-tag-distrib')) {
             this.nondistribWrap.style.display = 'none';
             this.distribWrap.style.display = 'block';
+            this.customerWrap.style.display = 'none';
+          } else if (tag.classList.contains('js-tag-customer')) {
+            this.nondistribWrap.style.display = 'none';
+            this.customerWrap.style.display = 'block';
+            this.distribWrap.style.display = 'none';
           } else {
             this.nondistribWrap.style.display = 'block';
             this.distribWrap.style.display = 'none';
+            this.customerWrap.style.display = 'none';
           }
         }
+
+        /*if (tag.classList.contains('js-tag-distrib') && tag.classList.contains('tag--active')) {
+          this.nondistribWrap.style.display = 'none';
+          this.distribWrap.style.display = 'block';
+          this.customerWrap.style.display = 'none';
+        } else {
+          this.nondistribWrap.style.display = 'block';
+          this.distribWrap.style.display = 'none';
+        }
+
+        if (tag.classList.contains('js-tag-customer')) {
+          this.nondistribWrap.style.display = 'none';
+          this.customerWrap.style.display = 'block';
+          this.distribWrap.style.display = 'none';
+        } else {
+          this.nondistribWrap.style.display = 'block';
+          this.customerWrap.style.display = 'none';
+        }*/
       });
     });
   }
