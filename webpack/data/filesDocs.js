1ee36ed438f0a030b3c81892a97a3fdf37a0d71e
Evgen Skirko <conkurent.evgen@gmail.com>
2020-05-09 20:27:49 +0300

    Docs page

---
+++ webpack/data/filesDocs.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    type: 'zip',
+    href: 'javascript:void(0);',
+    fileName: 'Скачать архив 2020.zip',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '17.08.2019 в 13:32',
+    action: true
+  },
+  {
+    type: 'pdf',
+    href: 'javascript:void(0);',
+    fileName: 'Знаки безопасности.pdf',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '',
+    action: true
+  },
+  {
+    type: 'zip',
+    href: 'javascript:void(0);',
+    fileName: 'Автономные системы аварийного освещения.zip',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '17.08.2019 в 13:32',
+    action: true
+  },
+  {
+    type: 'xls',
+    href: 'javascript:void(0);',
+    fileName: 'Централизованные технические решения.xlsx',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '17.08.2019 в 13:32',
+    action: true
+  },
+  {
+    type: 'xls',
+    href: 'javascript:void(0);',
+    fileName: 'Блоки аварийного питания STABILAR-2, INEXI-2.xlsx',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '17.08.2019 в 13:32',
+    action: true
+  },
+  {
+    type: 'doc',
+    href: 'javascript:void(0);',
+    fileName: 'Название документа 1.docx',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '',
+    action: true
+  }
+];
---
+++ webpack/data/filesMore.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    type: 'doc',
+    href: 'javascript:void(0);',
+    fileName: 'Автономные технические решения.docx',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '',
+    action: true
+  },
+  {
+    type: 'zip',
+    href: 'javascript:void(0);',
+    fileName: 'Автономные системы аварийного освещения.zip',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '17.08.2019 в 13:32',
+    action: true
+  },
+  {
+    type: 'xls',
+    href: 'javascript:void(0);',
+    fileName: 'Полный каталог продукции компании 2018.xlsx',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '17.08.2019 в 13:32',
+    action: true
+  },
+  {
+    type: 'pdf',
+    href: 'javascript:void(0);',
+    fileName: 'Знаки безопасности.pdf',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '',
+    action: true
+  },
+  {
+    type: 'xls',
+    href: 'javascript:void(0);',
+    fileName: 'Блоки аварийного питания STABILAR-2, INEXI-2.xlsx',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '17.08.2019 в 13:32',
+    action: true
+  },
+  {
+    type: 'doc',
+    href: 'javascript:void(0);',
+    fileName: 'Автономные технические решения для отрасли 2018-2019.docx',
+    size: '(7.5 Mb)',
+    create: '16.08.2019 в 13:32',
+    replace: '',
+    action: true
+  }
+];
