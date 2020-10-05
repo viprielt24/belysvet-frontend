87c412decdd6a0c947af71721d86d822fb45c008
dsmirnov <dsmirnov@centerfield.com>
2020-10-05 19:34:18 +0300

    Авторизованный пользователь - меню

---
+++ src/components/header-nonauthorized/header-nonauthorized.js
@@ -0,0 +1,104 @@
+export default class {
+  constructor() {
+    this.isMobile = document.documentElement.classList.contains('mobile');
+    this.parentMenu = document.querySelectorAll('.js-header__menu-parent');
+    this.parentTopMenu = document.querySelectorAll('.js-header__top-parent');
+
+    if (this.isMobile) {
+      this.mobileMenu = document.querySelector('.js-header-mobile');
+      this.mobileMenuBtn = document.querySelector('.header__middle .js-header__burger');
+      this.mobileMenuMainBtn = this.mobileMenu.querySelector('.js-header__burger');
+
+      this.mobileMenuSearchBtn = document.querySelector('.js-header-mobile-search-open');
+      this.mobileMenuSearch = this.mobileMenu.querySelector('.js-header-mobile__search');
+    }
+
+    this.addEvents();
+  }
+
+  addEvents() {
+    this.menuToggle();
+    this.topMenuToggle();
+
+    if (this.isMobile) {
+      this.mobileMenuSearchBtn.addEventListener('click', () => {
+        this.mobileSearchToggle();
+      });
+
+      this.mobileMenuBtn.addEventListener('click', () => {
+        this.mobileMenuToggle();
+      });
+
+      this.mobileMenuMainBtn.addEventListener('click', () => {
+        this.mobileMenuToggle();
+      });
+    }
+  }
+
+  menuToggle() {
+    this.parentMenu.forEach(menu => {
+      menu.addEventListener('click', () => {
+        if (menu.classList.contains('is-open')) {
+          menu.classList.remove('is-open');
+        } else {
+          let openMenu = document.querySelector('.js-header__menu-parent.is-open');
+          if (openMenu) {
+            openMenu.classList.remove('is-open');
+          }
+          menu.classList.add('is-open');
+        }
+      });
+
+      this.parentTopMenu.forEach(menu => {
+        if (menu.classList.contains('is-open')) {
+          menu.classList.remove('is-open');
+        }
+      });
+    });
+  }
+
+  topMenuToggle() {
+    this.parentTopMenu.forEach(menu => {
+      menu.addEventListener('click', () => {
+        if (menu.classList.contains('is-open')) {
+          menu.classList.remove('is-open');
+        } else {
+          let openMenu = document.querySelector('.js-header__top-parent.is-open');
+          if (openMenu) {
+            openMenu.classList.remove('is-open');
+          }
+          menu.classList.add('is-open');
+        }
+
+        this.parentMenu.forEach(menu => {
+          if (menu.classList.contains('is-open')) {
+            menu.classList.remove('is-open');
+          }
+        });
+      });
+    });
+  }
+
+  mobileMenuToggle() {
+    if (this.mobileMenu.classList.contains('is-open')) {
+      this.mobileMenu.classList.remove('is-open');
+      this.mobileMenuBtn.classList.remove('is-active');
+      this.mobileMenuMainBtn.classList.remove('is-active');
+      setTimeout(() => {
+        this.mobileMenuSearch.classList.remove('is-open');
+      }, 400);
+    } else {
+      this.mobileMenu.classList.add('is-open');
+      this.mobileMenuBtn.classList.add('is-active');
+      this.mobileMenuMainBtn.classList.add('is-active');
+    }
+  }
+
+  mobileSearchToggle() {
+    if (this.mobileMenuSearch.classList.contains('is-open')) {
+      this.mobileMenuSearch.classList.remove('is-open');
+    } else {
+      this.mobileMenuSearch.classList.add('is-open');
+    }
+  }
+}
