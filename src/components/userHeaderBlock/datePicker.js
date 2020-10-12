4378925a4bd9d8196602ec33dad1cb4336147ecb
dsmirnov <dsmirnov@centerfield.com>
2020-10-12 22:01:38 +0300

    Создан Datepicker

---
+++ src/components/userHeaderBlock/datePicker.js
@@ -0,0 +1,30 @@
+import datepicker from 'js-datepicker'
+//const picker = document.querySelector('.uhbData__dateCalendar')
+
+export default class {
+  constructor() {
+    this.datePicker = document.querySelectorAll('.uhbData__dateCalendar')
+    this.dateList = document.querySelector('.uhbData__dateList')
+    this.addEvent();
+  }
+
+  addEvent() {
+    this.datePicker.forEach(datePick => {
+      datePick.addEventListener('click', (e) => {
+        const newDatePicker =  datepicker('.uhbData__dateCalendar', {
+          customMonths: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+          customDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+          onSelect: instance => {
+            // Show which date was selected.
+            console.log(instance.dateSelected.toLocaleDateString())
+            const newDateItem = instance.dateSelected.toLocaleDateString().split('/').join('.');
+            let newItem = document.createElement('div')
+            newItem.innerHTML = newDateItem
+            newItem.classList.add('uhbData__dateItem')
+            this.dateList.append(newItem)
+          },
+        })
+      })
+    });
+  }
+}
